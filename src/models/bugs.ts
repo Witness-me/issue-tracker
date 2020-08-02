8ecbc05155af34f25bdec9fb4782b261e2e809b8
Vladislav_Nikolaenko <vladislavnikolaenko@gmail.com>
2020-08-02 18:05:40 +0300

    Add mongoose bug model

--- src/models/bugs.ts
+++ src/models/bugs.ts
@@ -1,1 +1,15 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
+const bugSchema = new Schema({
+  title: { type: String, required: [true, "Bug title is required"] },
+  status: { type: String, required: [true, "Choose the bug's status"] },
+  comments: String,
+  priority: String,
+  screenshot: Image,
+  deadline: Date,
+  isExpired: Boolean,
+});
+
+const Bug = mongoose.model("bug", bugSchema);
+module.exports = Bug;
