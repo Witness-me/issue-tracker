7106059b058d25ed75987e6d23b31a05727014c1
Vladislav_Nikolaenko <vladislavnikolaenko@gmail.com>
2020-09-07 21:31:18 +0300

    Configure backend API to accept RS256 tokens

--- server/src/app.ts
+++ server/src/app.ts
@@ -4,6 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const routes = require("./controllers/issues");
+const jwt = require("express-jwt");
+const jwks = require("jwks-rsa");
 import { connectDb } from "./models";
 
 const app = express();
@@ -11,9 +13,23 @@ const app = express();
 // Middleware
 // when app.use() is called with a function as it’s only argument, it’ll match every request and will act as middleware
 app.use(bodyParser.json()); // the function listens for req.on(‘data’) and constructs req.body from the chunks of data it gets
-app.use(cors({ origin: "*" }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
 const port = process.env.PORT || 5000;
+
+const jwtCheck = jwt({
+  secret: jwks.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: "https://witness-me.eu.auth0.com/.well-known/jwks.json",
+  }),
+  audience: "issue-tracker",
+  issuer: "https://witness-me.eu.auth0.com/",
+  algorithms: ["RS256"],
+});
+// app.use(jwtCheck);
 
 connectDb().then(async () => {
   app.listen(port, () => {
